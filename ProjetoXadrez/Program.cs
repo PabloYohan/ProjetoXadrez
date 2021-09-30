8f44f5fedec2f7db94bcd7b73876c2b5c239c7a3
Pablo Yohan <pabloyohandias@gmail.com>
2021-09-30 14:46:33 -0300

    Testes com movimentos possiveis das peças

--- ProjetoXadrez/Program.cs
+++ ProjetoXadrez/Program.cs
@@ -20,6 +20,13 @@ namespace ProjetoXadrez
 
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossiveis = partida.Tab.peca(origem).movimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.Tab, posicoesPossiveis);
+
+                    Console.WriteLine();
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
